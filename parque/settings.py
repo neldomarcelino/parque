273ec6ec8a67fb78f8068f212f61e73ca2fd754c
neldomarcelino <guerzeneldo@gmail.com>
2022-06-09 23:04:08 +0200

    runtime

--- parque/settings.py
+++ parque/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -194,7 +193,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = 'staticfiles'
 
-
+GDAL_VERSION = '2.3.0'
 
 LEAFLET_CONFIG = {
     # conf here
